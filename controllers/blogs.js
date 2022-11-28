9f7c97353550c1d240c3b22bc21a2caaef32278d
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2022-11-28 16:48:51 +0200

    Exercises 13.5&13.6: restructured and added liking

---
+++ controllers/blogs.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const { Blog } = require("../models");
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+  next();
+};
+
+/*** GET-REQUESTS ****/
+router.get("/", async (req, res) => {
+  const blogs = await Blog.findAll();
+  console.log(JSON.stringify(blogs, null, 2));
+  res.json(blogs);
+});
+router.get("/:id", blogFinder, async (req, res) => {
+  const blog = req.blog;
+  if (blog) {
+    console.log(blog.toJSON());
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+/*** PUT-REQUESTS ****/
+router.put("/:id", blogFinder, async (req, res) => {
+  const blog = req.blog;
+  if (blog) {
+    blog.likes = req.body.likes;
+    await blog.save();
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+/*** POST-REQUESTS ****/
+router.post("/", async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    return res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+/*** DELETE-REQUESTS ****/
+router.delete("/:id", blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy();
+  }
+  res.status(204).end();
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,98 +1,20 @@
-require("dotenv").config();
-const { Sequelize, Model, DataTypes } = require("sequelize");
-const express = require("express");
-const app = express();
-app.use(express.json());
+const express = require('express')
+const app = express()
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
 
-class Blog extends Model {}
+const blogsRouter = require('./controllers/blogs')
 
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    author: {
-      type: DataTypes.TEXT,
-    },
-    url: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "blog",
-  }
-);
+app.use(express.json())
 
-/************************ENDPOINTS***********************/
+app.use('/api/blogs', blogsRouter)
 
-/*** GET-REQUESTS ****/
-app.get("/api/blogs", async (req, res) => {
-  const blogs = await Blog.findAll();
-  console.log(JSON.stringify(blogs, null, 2))
-  res.json(blogs);
-});
-app.get("/api/blogs/:id", async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id);
-  if (blog) {
-    console.log(blog.toJSON())
-    res.json(blog);
-  } else {
-    res.status(404).end();
-  }
-});
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
 
-/*** PUT-REQUESTS ****/
-app.put("/api/blogs/:id", async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id);
-  if (blog) {
-    blog.likes = blog.likes + 1;
-    await blog.save();
-    res.json(blog);
-  } else {
-    res.status(404).end();
-  }
-});
-
-/*** POST-REQUESTS ****/
-app.post("/api/blogs", async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body);
-    return res.json(blog);
-  } catch (error) {
-    return res.status(400).json({ error });
-  }
-});
-
-/*** DELETE-REQUESTS ****/
-app.delete("/api/blogs/:id", async (req, res) => {
-  try {
-    const blog = await Blog.findByPk(req.params.id);
-    blog.destroy();
-    return res.status(204).end();
-  } catch (error) {
-    return res.status(400).json({ error });
-  }
-});
-
-/********************CONFIGURATION**********************/
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+start()
