12be1bc0700525907f2153fa72065fc0fa0009ca
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2022-11-28 17:26:07 +0200

    Exercise 13.8: Added user support

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const app = express()
 const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const errorHandler = require('./util/middleware')
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
 app.use(errorHandler)
 
 const start = async () => {
